4e895b6c5e403a80b632fb055a9e0a06bec93e03
Martin <jacobmartin03@hotmail.com>
2025-12-21 13:31:55 +0100

    feat: Add some SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,25 @@ export default defineNuxtConfig({
     "nuxt-security",
   ],
 
+  app: {
+    head: {
+      title: "CSLabs – Hackathon : Hack the Narrative",
+      meta: [
+        {
+          id: "description",
+          name: "description",
+          content: "Le Hackathon du CSLabs est un événement de 48 heures où des passionnés de technologie se réunissent pour collaborer, innover et créer des projets technologiques impressionnants.",
+        },
+      ],
+      link: [
+        {rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
+        {rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
+        {rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+        {rel: "manifest", href: "/site.webmanifest"},
+      ],
+    },
+  },
+
   // TODO: Is this still necessary?
   postcss: {
     plugins: {
