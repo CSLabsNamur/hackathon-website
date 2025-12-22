98915685d70367852d5fa86c2590803c2a382b2c
Martin <jacobmartin03@hotmail.com>
2025-12-22 01:26:18 +0100

    style(lint): Configure ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
+export default withNuxt({
+    rules: {
+      "vue/first-attribute-linebreak": "off",
+      "vue/require-default-prop": "off",
+      "vue/no-multiple-template-root": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    }
+  }
 )
