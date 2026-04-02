c2989057fa968212cfe0a99dd37beddd86695425
Martin <jacobmartin03@hotmail.com>
2026-04-02 21:51:58 +0200

    fix: Fix search query watcher

--- app/composables/useSearchQuery.ts
+++ app/composables/useSearchQuery.ts
@@ -2,7 +2,7 @@ export const useSearchQuery = () => {
   const route = useRoute();
   const filter = ref("");
 
-  watchOnce(() => route.query["search"], (value) => {
+  watch(() => route.query["search"], (value) => {
     filter.value = (Array.isArray(value) ? value[0] : value) ?? "";
   }, {
     immediate: true,
