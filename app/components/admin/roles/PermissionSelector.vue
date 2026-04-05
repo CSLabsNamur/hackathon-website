5f9f10dfd7b3ca97ea5a802b35722e224670cc9a
Martin <jacobmartin03@hotmail.com>
2026-04-05 02:26:20 +0200

    feat(roles): Add "select all" toggle for permission groups

--- app/components/admin/roles/PermissionSelector.vue
+++ app/components/admin/roles/PermissionSelector.vue
@@ -28,20 +28,52 @@ function setPermissionEnabled(permissionKey: string, enabled: boolean) {
 
   model.value = Array.from(selectedKeys).sort();
 }
+
+const selectAllCheckboxModel = computed(() => (groupPermissions: PermissionDb[]) => {
+  const selectedCount = groupPermissions.filter(permission => model.value.includes(permission.key)).length;
+  if (selectedCount === 0) {
+    return false;
+  } else if (selectedCount === groupPermissions.length) {
+    return true;
+  } else {
+    return "indeterminate";
+  }
+});
+
+function setPermissionEnabledForGroup(group: string, enabled: boolean) {
+  const groupPermissions = groupedPermissions.value[group] || [];
+  const selectedKeys = new Set(model.value);
+
+  groupPermissions.forEach(permission => {
+    if (enabled) {
+      selectedKeys.add(permission.key);
+    } else {
+      selectedKeys.delete(permission.key);
+    }
+  });
+
+  model.value = Array.from(selectedKeys).sort();
+}
 </script>
 
 <template>
   <div class="space-y-4">
     <div v-for="(groupPermissions, group) in groupedPermissions" :key="group"
          class="rounded-lg border border-default p-4 space-y-3">
-      <p class="text-sm font-semibold uppercase text-muted">
-        {{ group }}
-      </p>
+      <div class="flex justify-between">
+        <p class="text-sm font-semibold uppercase text-muted">
+          {{ group }}
+        </p>
+
+        <UCheckbox :id="`select-all-${group}`" :model-value="selectAllCheckboxModel(groupPermissions)"
+                   :disabled="disabled" label="Tout sélectionner"
+                   @update:model-value="setPermissionEnabledForGroup(group, $event as boolean)"/>
+      </div>
 
       <div class="grid gap-3 md:grid-cols-2">
-        <UCheckbox v-for="permission in groupPermissions" :key="permission.key"
-                   :model-value="model.includes(permission.key)" :disabled="disabled" :label="permission.name"
-                   :description="permission.key"
+        <UCheckbox v-for="permission in groupPermissions" :id="`permission-${permission.id}`"
+                   :key="permission.id" :model-value="model.includes(permission.key)"
+                   :disabled="disabled" :label="permission.name" :description="permission.key"
                    @update:model-value="setPermissionEnabled(permission.key, $event as boolean)"/>
       </div>
     </div>
