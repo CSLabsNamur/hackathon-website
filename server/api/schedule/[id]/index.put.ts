9e4bc201d3f9b2b7ca4c312689e356e152903de1
Martin <jacobmartin03@hotmail.com>
2026-04-06 23:57:34 +0200

    feat(schedule): Add checks for schedule item saving

--- server/api/schedule/[id]/index.put.ts
+++ server/api/schedule/[id]/index.put.ts
@@ -1,12 +1,15 @@
 import schema from "#shared/schemas/schedule/edit";
 import * as v from "valibot";
 import idParamSchema from "#shared/schemas/id";
+import { assertScheduleItemCanBeSaved } from "#server/utils/scheduleItems";
 
 export default defineEventHandler(async (event) => {
   await requirePermission(event, "schedule.update");
   const {id: scheduleItemId} = await getValidatedRouterParams(event, v.parser(idParamSchema));
 
   const data = await readValidatedBody(event, v.parser(schema));
+
+  await assertScheduleItemCanBeSaved(data, scheduleItemId);
 
   return prisma.scheduleItem.update({where: {id: scheduleItemId}, data});
 });
--- server/api/schedule/index.post.ts
+++ server/api/schedule/index.post.ts
@@ -1,10 +1,13 @@
 import schema from "#shared/schemas/schedule/create";
 import * as v from "valibot";
+import { assertScheduleItemCanBeSaved } from "#server/utils/scheduleItems";
 
 export default defineEventHandler(async (event) => {
   await requirePermission(event, "schedule.create");
 
   const data = await readValidatedBody(event, v.parser(schema));
+
+  await assertScheduleItemCanBeSaved(data);
 
   return prisma.scheduleItem.create({data});
 });
