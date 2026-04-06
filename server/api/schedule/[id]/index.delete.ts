9b2a112beeeae00269eea3f069c43bbe05c160d1
Martin <jacobmartin03@hotmail.com>
2026-04-06 23:49:44 +0200

    refactor(api): Move old [id] files under [id] folder

--- server/api/schedule/[id]/index.delete.ts
+++ server/api/schedule/[id]/index.delete.ts
@@ -6,5 +6,10 @@ export default defineEventHandler(async (event) => {
 
   const {id} = await getValidatedRouterParams(event, v.parser(idParamSchema));
 
+  const scheduleItem = await prisma.scheduleItem.findUnique({where: {id}});
+  if (!scheduleItem) {
+    throw createError({statusCode: 404, message: "Partie de planning introuvable"});
+  }
+
   return prisma.scheduleItem.delete({where: {id}});
 });
